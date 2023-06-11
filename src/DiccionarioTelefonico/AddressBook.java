1d11d0b4f98fdbdecce9cdd4a655a714fc67552c
Sam <samuel.knd.9@gmail.com>
2023-06-11 14:02:42 -0600

    Version 1

--- src/DiccionarioTelefonico/AddressBook.java
+++ src/DiccionarioTelefonico/AddressBook.java
@@ -1,2 +1,99 @@
-package DiccionarioTelefonico;public class AddressBook {
+package DiccionarioTelefonico;
+
+import com.opencsv.*;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.FileNotFoundException;
+import java.io.*;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class AddressBook {
+    HashMap<String, String> directorio;
+    String archCSV = ".\\src\\DiccionarioTelefonico\\Files\\Diccionario.csv";
+
+    public AddressBook() {
+        directorio = new HashMap<String, String>();
+    }
+    public boolean Load() {
+        boolean result=false;
+        String[] direccion;
+        try {
+            Scanner sc = new Scanner(new File(archCSV));
+            sc.useDelimiter(",");   //sets the delimiter pattern
+            while (sc.hasNextLine())  //returns a boolean value
+            {
+                direccion= sc.nextLine().split(",");
+                Create(direccion[0],direccion[1]);
+            }
+            sc.close();
+
+            result = true;
+        }catch (Exception e) {
+            result = false;
+        }
+
+        return result;
+    }
+
+    public  String Save(){
+        String result="";
+        String eol = System.getProperty("line.separator");
+        try {
+            Writer writer = new FileWriter(archCSV);
+            for (Map.Entry<String, String> entry : directorio.entrySet()) {
+                writer.append(entry.getKey())
+                        .append(',')
+                        .append(entry.getValue())
+                        .append(eol);
+            }
+            writer.close();
+            result= "Se han guardado los cambios";
+        }catch (Exception e){
+            result ="Ocurrio un problema al guardar los cambios";
+        }
+
+        return result;
+    }
+
+    public  String List(){
+        String result="";
+        try {
+            for (String i : directorio.keySet()) {
+                System.out.println("Numero: " + i + " Nombre: " + directorio.get(i));
+            }
+        }catch (Exception e){
+            result ="Ocurrio un problema al intentar mostrar los contactos";
+        }
+
+        return result;
+    }
+
+    public String Create(String numero, String nombre){
+        String result="";
+        try {
+            directorio.put(numero,nombre);
+            result= "El contacto se ha agregado";
+        }catch (Exception e){
+            result ="Ocurrio un problema al crear el contacto";
+        }
+
+        return result;
+    }
+
+    public  String Delete(String numero){
+        String result="";
+        try {
+            directorio.remove(numero);
+            result= "El contacto se ha borrado";
+        }catch (Exception e){
+            result ="Ocurrio un problema al borrar el contacto";
+        }
+
+        return result;
+    }
 }
--- src/DiccionarioTelefonico/Main.java
+++ src/DiccionarioTelefonico/Main.java
@@ -1,17 +1,68 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+package DiccionarioTelefonico;
+
+import com.opencsv.exceptions.CsvValidationException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
+    static AddressBook addressBook = new AddressBook();
+    public static void main(String[] args) throws CsvValidationException, IOException {
+        if (addressBook.Load()){
+            Menu();
+        }else{
+            System.out.println("Ocurrio un problema al cargar los contactos");
+        }
+    }
+    public static void Menu() throws IOException {
+        String opcion="",numero="",nombre="";
+        System.out.println("¡Bienvenido!\n¿Que deseas hacer?\n 1) Ver Contactos\n 2) Agregar Contacto\n 3) Eliminar Contacto\n 4) Salir");
+        opcion = scanner.readLine();
+        switch(opcion.toUpperCase()) {
+            case "1": {
+                addressBook.List();
+                Menu();
+                break;
+            }
+            case "2": {
+                System.out.println("Ingresa el numero de telefono");
+                numero = scanner.readLine();
+                System.out.println("Ingresa el nombre de tu contacto");
+                nombre = scanner.readLine();
+                if (numero != "" && nombre != ""){
+                    if (numero.length()==10){
+                        System.out.println(addressBook.Create(numero,nombre));
+                    }else{
+                        System.out.println("Numero de telefono invalido");
+                    }
+                }else {
+                    System.out.println("Ningun campo puede quedar vacio");
+                }
+                Menu();
+                break;
+            }
+            case "3": {
+                System.out.println("Ingresa el numero de telefono");
+                numero = scanner.readLine();
+                if (numero != ""){
+                    System.out.println(addressBook.Delete(numero));
+                }else {
+                    System.out.println("Ningun campo puede quedar vacio");
+                }
+                Menu();
+                break;
+            }
+            case "4": {
+                System.out.println(addressBook.Save());
+                System.out.println("Adios");
+                break;
+            }
+            default: {
+                System.out.println("Opcion invalida");
+                Menu();
+                break;
+            }
         }
     }
 }
